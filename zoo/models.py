7a82f7c3f5f4a0e025a22ea391945396f8fa3880
Adib Tajouri <tadib24@gmail.com>
2024-08-05 20:36:06 +0200

    change the superkraft feld

--- zoo/models.py
+++ zoo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Hologram(models.Model):
     name = models.CharField(max_length=100)
     gewicht = models.CharField(max_length=100)
-    superkraft = models.TextField()
+    superkraft = models.CharField(max_length=100)
     ausgestorben_seit = models.DateField(null=True, blank=True)
 
     def __str__(self):
--- zoo/views.py
+++ zoo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from .models import Hologram
 from .forms import HologramForm
+from django.contrib import messages
 
 def index(request):
     holograms = Hologram.objects.all()
@@ -11,7 +12,8 @@ def add_hologram(request):
     if request.method == 'POST':
         form = HologramForm(request.POST)
         if form.is_valid():
-            form.save()
+            hologram = form.save()
+            messages.success(request, f'Das Hologramm "{hologram.name}" wurde erfolgreich hinzugefügt.')
             return redirect('index')
     else:
         form = HologramForm()
@@ -22,7 +24,8 @@ def edit_hologram(request, id):
     if request.method == 'POST':
         form = HologramForm(request.POST, instance=hologram)
         if form.is_valid():
-            form.save()
+            hologram = form.save()
+            messages.success(request, f'Die Änderungen im "{hologram.name}" wurden erfolgreich gespeichert.')
             return redirect('index')
     else:
         form = HologramForm(instance=hologram)
@@ -32,5 +35,6 @@ def delete_hologram(request, id):
     hologram = get_object_or_404(Hologram, id=id)
     if request.method == 'POST':
         hologram.delete()
+        messages.success(request, f'Das Hologramm "{hologram.name}" wurde erfolgreich gelöscht.')
         return redirect('index')
     return render(request, 'delete_hologram.html', {'hologram': hologram})
